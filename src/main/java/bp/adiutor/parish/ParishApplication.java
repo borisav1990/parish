9a7b506d7e0b85fdaa17aa42e7e571b10ad0d91f
borisav1990 <borisavpopovic@hotmail.com>
2021-08-25 11:22:35 +0200

    my comm

---
+++ src/main/java/bp/adiutor/parish/ParishApplication.java
@@ -0,0 +1,13 @@
+package bp.adiutor.parish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParishApplication.class, args);
+	}
+
+}
